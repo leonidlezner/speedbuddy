fbd65fddd057e225d55aa53c5c563189f2e8907f
Leonid Lezner <info@leonidlezner.com>
2016-10-05 20:36:55 +0200

    Initial commit

---
+++ import_geodata.py
@@ -0,0 +1,75 @@
+import csv
+from bson.son import SON
+import pymongo
+from pymongo import MongoClient
+
+
+class GeoFormatException(Exception):
+    pass
+
+class GeodataImporter:
+    def __init__(self, db_collection=None):
+        self.total_points = 0
+        self.db_collection = db_collection
+
+    def import_row(self, row):
+        if self.db_collection is not None:
+          self.db_collection.insert({
+            'name': row[2],
+            'speed': row[3],
+            'mobile': row[4],
+            'loc': {
+              'type': 'Point',
+              'coordinates': [row[0], row[1]],
+            }
+          })
+
+    def load(self, filename, speed=0, mobile=False):
+        # Open the csv file and iterate through the lines
+        with open(filename, 'r', encoding="ISO-8859-1") as csvfile:
+            row_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+            for row in row_reader:
+                # Check the number of columns
+                if len(row) != 3:
+                    raise GeoFormatException("Wrong geo data format: {}".format(', '.join(row)))
+
+                # Create a tuple with geo coordinates
+                geo_point = (float(row[0]), float(row[1]), row[2], speed, mobile)
+
+                # Count up the point counter
+                self.total_points += 1
+
+                # Process the row
+                self.import_row(geo_point)
+
+
+
+
+
+def import_demo():
+    mongo = MongoClient()
+    database = mongo['speedcams']
+    
+    database.points.drop()
+    
+    points = database.points
+    
+    points.ensure_index([("loc", pymongo.GEOSPHERE)])
+    
+    
+    importer = GeodataImporter(db_collection=points)
+
+    importer.load('./garmin-mobil/Blitzer_mobil.csv', speed=0, mobile=True)
+
+    for speed in range(10, 140, 10):
+        importer.load('./garmin-mobil/Blitzer_mobil_{}.csv'.format(speed), speed=speed, mobile=True)
+
+    importer.load('./garmin-mobil/Blitzer_mobil_Abstand.csv', speed=0, mobile=True)
+    
+
+
+
+if __name__ == '__main__':
+    import_demo()
+
---
+++ search_position.py
@@ -0,0 +1,62 @@
+from bson.son import SON
+import pymongo
+from pymongo import MongoClient
+
+import math
+
+# Source http://www.johndcook.com/blog/python_longitude_latitude/
+def distance_on_unit_sphere(lat1, long1, lat2, long2):
+  # Convert latitude and longitude to
+  # spherical coordinates in radians.
+  degrees_to_radians = math.pi/180.0
+ 
+  # phi = 90 - latitude
+  phi1 = (90.0 - lat1)*degrees_to_radians
+  phi2 = (90.0 - lat2)*degrees_to_radians
+ 
+  # theta = longitude
+  theta1 = long1*degrees_to_radians
+  theta2 = long2*degrees_to_radians
+ 
+  # Compute spherical distance from spherical coordinates.
+ 
+  # For two locations in spherical coordinates
+  # (1, theta, phi) and (1, theta', phi')
+  # cosine( arc length ) =
+  # sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+  # distance = rho * arc length
+ 
+  cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) +
+  math.cos(phi1)*math.cos(phi2))
+  arc = math.acos( cos )
+ 
+  # Remember to multiply arc by the radius of the earth
+  # in your favorite set of units to get length.
+  return arc * 6371 * 1000
+
+def main():
+  mongo = MongoClient()
+  database = mongo['speedcams']
+    
+  points = database.points
+  
+  coordinates = [47.532554, 8.709355]
+  
+  coordinates.reverse()
+  
+  max_distance = 500
+
+  query = {
+    'loc':
+      {'$near': SON([('$geometry', SON([('type', 'Point'), ('coordinates', coordinates)])), ('$maxDistance', max_distance)])
+    }
+  }
+  
+  print('Searching...')
+  
+  for point in points.find(query):
+    print(point['loc']['coordinates'], distance_on_unit_sphere(point['loc']['coordinates'][1], point['loc']['coordinates'][0], coordinates[1], coordinates[0]))
+  
+      
+if __name__ == '__main__':
+  main()
